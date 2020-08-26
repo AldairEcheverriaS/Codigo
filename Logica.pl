6869582cdbc42fc289725fc6dc877648bbd1c3d4
Aldair Echeverria Sanabria <70154683+AldairEcheverriaS@users.noreply.github.com>
2020-08-26 10:08:52 -0400

    Add files via upload

---
+++ Logica.pl
@@ -0,0 +1,8 @@
+/* Predicados monádicos: PROPIEDADES */
+/* mujer(Per) <- Per es una mujer */ 
+mujer(clara).
+mujer(chelo).
+/*hombre(Per)<- Per es un hombre */ 
+hombre(jorge). 
+hombre(felix). 
+hombre(borja).
